6a3c92c57574a8a2b4f8457609a71a2f36609935
Berk Batuhan Mengüç <54108066+berkmenguc@users.noreply.github.com>
2024-07-21 16:29:35 +0300

    First Upload

---
+++ spending-tracking-system/src/main/java/com/example/spending_tracking_system/SpendingTrackingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.spending_tracking_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpendingTrackingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpendingTrackingSystemApplication.class, args);
+	}
+
+}
---
+++ spending-tracking-system/src/test/java/com/example/spending_tracking_system/SpendingTrackingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spending_tracking_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpendingTrackingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
